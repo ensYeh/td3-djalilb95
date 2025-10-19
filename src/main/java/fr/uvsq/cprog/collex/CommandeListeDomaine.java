a73c6b873a6182d4e994ee9319544ac6b4dc8974
djalil benyelles <djalil@Mac.lan>
2025-10-19 12:03:13 +0200

    Ajout de la commande qui recherche un domaine

---
+++ src/main/java/fr/uvsq/cprog/collex/CommandeListeDomaine.java
@@ -0,0 +1,31 @@
+package fr.uvsq.cprog.collex;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/** Commande : liste les machines appartenant à un domaine donné. */
+public class CommandeListeDomaine implements Commande {
+
+  private final String domaine;
+  private final boolean trierParAdresse;
+
+  public CommandeListeDomaine(final String domaine, final boolean trierParAdresse) {
+    this.domaine = domaine;
+    this.trierParAdresse = trierParAdresse;
+  }
+
+  @Override
+  public String execute(final Dns dns) {
+    List<DnsItem> items = dns.getItems(domaine, trierParAdresse);
+    if (items.isEmpty()) {
+      return "(aucune entrée pour le domaine " + domaine + ")";
+    }
+
+    return items.stream()
+        .map(it -> trierParAdresse
+            ? it.ip().value() + " " + it.nom().value()
+            : it.nom().value() + " " + it.ip().value())
+        .collect(Collectors.joining("\n"));
+  }
+}
+
