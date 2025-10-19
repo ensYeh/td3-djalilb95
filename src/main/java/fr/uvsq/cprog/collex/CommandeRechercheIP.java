943cce43b80d6d9c57ed6a070722902c894256b9
djalil benyelles <djalil@Mac.lan>
2025-10-19 12:01:29 +0200

    Ajout de la recherche d'IP

---
+++ src/main/java/fr/uvsq/cprog/collex/CommandeRechercheIP.java
@@ -0,0 +1,21 @@
+package fr.uvsq.cprog.collex;
+
+/** Commande : rechercher l'adresse IP d'une machine donnée. */
+public class CommandeRechercheIP implements Commande {
+
+  private final NomMachine nom;
+
+  public CommandeRechercheIP(final String fqdn) {
+    this.nom = new NomMachine(fqdn);
+  }
+
+  @Override
+  public String execute(final Dns dns) {
+    DnsItem item = dns.getItem(nom);
+    if (item == null) {
+      return "ERREUR : nom inconnu";
+    }
+    return item.ip().value();
+  }
+}
+
